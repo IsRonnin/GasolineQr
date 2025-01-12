526284d54622a17de010b0b7e3db88563d44254f
артём ващенко <artemvasenko09@mail.ru>
2025-01-12 19:26:57 +0300

    added config

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy_garden.qrcode import QRCodeWidget
+from kivy.storage.jsonstore import JsonStore
+
+# Конфигурационный файл для сохранения данных
+CONFIG_FILE = "config.json"
+store = JsonStore(CONFIG_FILE)
 
 # Словари для хранения данных
 user_credentials = {
@@ -42,6 +47,8 @@ class LoginScreen(Screen):
         password = self.password_input.text
 
         if username in user_credentials and user_credentials[username] == password:
+            # Сохранение последнего пользователя в конфиг
+            store.put("last_user", username=username)
             self.manager.current = 'qr_screen'
             self.manager.get_screen('qr_screen').generate_qr(username)
         else:
@@ -76,6 +83,14 @@ class MyApp(App):
         sm = ScreenManager()
         sm.add_widget(LoginScreen(name='login_screen'))
         sm.add_widget(QRScreen(name='qr_screen'))
+
+        # Загрузка конфигурации
+        if store.exists("last_user"):
+            last_user = store.get("last_user")["username"]
+            if last_user and last_user in user_data:
+                sm.current = 'qr_screen'
+                sm.get_screen('qr_screen').generate_qr(last_user)
+
         return sm
 
 if __name__ == '__main__':
